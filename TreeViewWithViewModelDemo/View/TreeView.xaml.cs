d99ee406b01356a530fe60bb594d8ebc2ebc2807
ARS-CSGCL-DT <dennis.timlin@ars.usda.gov>
2016-10-20 16:18:29 -0400

    had to make the project x86

---
+++ TreeViewWithViewModelDemo/View/TreeView.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TreeViewWithViewModelDemo.View
+{
+    /// <summary>
+    /// Interaction logic for TreeView.xaml
+    /// </summary>
+    public partial class TreeView : Window
+    {
+        public TreeView()
+        {
+            InitializeComponent();
+            ViewModel.MainViewModel TreeView = new ViewModel.MainViewModel();
+            DataContext = new ViewModel.MainViewModel();
+        }
+    }
+}
